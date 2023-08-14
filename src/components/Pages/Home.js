4c47d981132a18ede0159498c129dddbdae2344a
Fabiha Qasim <fqasim@egenienext.com>
2023-08-14 12:42:46 +0500

    Added new changes

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Row } from 'antd';
+import GlaxyUltra from "./GlaxyUltra";
+
+ 
+
+
+
+
+
+
+
+export const Home = () => {
+  return (
+    <div className="image-grid">
+      <h4>Result</h4>
+  
+      <div className="grid">
+        <GlaxyUltra />
+      </div>
+    </div>
+  );
+};
+
+
+
+// 
